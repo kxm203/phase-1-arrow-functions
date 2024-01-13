514fbe68f949f9abfa20d390101b56c61fece565
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-01-12 20:23:43 -0500

    second try to git push

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const divide = (number1, number2) => number1 / number2;
+divide(2000, 100);
+
+const square = (number1) => number1 * number1;
+square(2);
+
+const add = (number1, number2) => number1 + number2;
+add(3,4)
